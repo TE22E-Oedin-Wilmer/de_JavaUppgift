df7f72b8333c85ebb4189f5af3effe31a29d0fcb
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-02-28 16:38:12 +0100

    Commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function Watch1() {
-    document.getElementById("Watch1").innerHTML = "Hello World";
+let Watch1 = 0
+let Watch2 = 0
+let Watch3 = 0
+let Watch4 = 0
+let Watch5 = 0
+let Watch6 = 0
+
+const watch1click =()=> {
+    Watch1++;
+    console.log("Watch1 added");
+    console.log (Watch1);
+}
+if (Watch1 < 1) {
+    document.getElementById(Watch1cart).style.display="none";
 }
