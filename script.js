172d52391006db074e41031c2b0af1b918e6371b
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-03-03 13:40:28 +0100

    We are getting somewhere

---
+++ script.js
@@ -0,0 +1,39 @@
+var content1 = 0;
+var content2 = 0;
+var content3 = 0;
+var content4 = 0;
+var content5 = 0;
+var content6 = 0;
+
+
+function add(num) {
+    num = 1;
+}
+
+function remove(rnum){
+    rnum = 0;
+}
+
+if (content1 > 0) {
+    document.getElementById("drp1").style.display="block";
+}
+
+if (content2 > 0) {
+    document.getElementById("drp2").style.display="block";
+}
+
+if (content3 > 0) {
+    document.getElementById("drp3").style.display="block";
+}
+
+if (content4 > 0) {
+    document.getElementById("drp4").style.display="block";
+}
+
+if (content5 > 0) {
+    document.getElementById("drp5").style.display="block";
+}
+
+if (content6 > 0) {
+    document.getElementById("drp6").style.display="block";
+}
