d82260eeda3841f01f11aad43f40bc16483b2826
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-03-04 09:49:24 +0100

    Doneso

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-var content1 = 0;
-var content2 = 0;
-var content3 = 0;
-var content4 = 0;
-var content5 = 0;
-var content6 = 0;
 
-
-function add(num) {
-    num = 1;
+function addtocart(productName) {
+    console.log(productName)
+    if (productName === 'Watch1') {
+        document.getElementById("drp1").style.display="block";
+    }
+    if (productName === 'Watch2') {
+        document.getElementById("drp2").style.display="block";
+    }
+    if (productName === 'Watch3') {
+        document.getElementById("drp3").style.display="block";
+    }
+    if (productName === 'Watch4') {
+        document.getElementById("drp4").style.display="block";
+    }
+    if (productName === 'Watch5') {
+        document.getElementById("drp5").style.display="block";
+    }
+    if (productName === 'Watch6') {
+        document.getElementById("drp6").style.display="block";
+    }
 }
 
-function remove(rnum){
-    rnum = 0;
-}
-
-if (content1 > 0) {
-    document.getElementById("drp1").style.display="block";
-}
-
-if (content2 > 0) {
-    document.getElementById("drp2").style.display="block";
-}
-
-if (content3 > 0) {
-    document.getElementById("drp3").style.display="block";
-}
-
-if (content4 > 0) {
-    document.getElementById("drp4").style.display="block";
-}
-
-if (content5 > 0) {
-    document.getElementById("drp5").style.display="block";
-}
-
-if (content6 > 0) {
-    document.getElementById("drp6").style.display="block";
+function remFromCart(productName){
+    console.log(productName)
+    if (productName === 'drp1') {
+        document.getElementById("drp1").style.display="none";
+    }
+    if (productName === 'drp2') {
+        document.getElementById("drp2").style.display="none";
+    }
+    if (productName === 'drp3') {
+        document.getElementById("drp3").style.display="none";
+    }
+    if (productName === 'drp4') {
+        document.getElementById("drp4").style.display="none";
+    }
+    if (productName === 'drp5') {
+        document.getElementById("drp5").style.display="none";
+    }
+    if (productName === 'drp6') {
+        document.getElementById("drp6").style.display="none";
+    }
 }
